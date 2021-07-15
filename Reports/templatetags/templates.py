82173db9b3f6b0168c656fdd7b745985419dafd4
Andrew Mikhalev <43449316+ITDarkUFO@users.noreply.github.com>
2021-07-15 13:01:06 +0500

    Настроена система шаблонов и класс meta

--- Reports/templatetags/templates.py
+++ Reports/templatetags/templates.py
@@ -1,3 +1,5 @@
+﻿from django.http import HttpResponse
+from django.conf import settings
 from django import template
 
 register = template.Library()
@@ -10,12 +12,18 @@ register = template.Library()
 # 	Тело функции
 
 
-@register.simple_tag(takes_context = False)
-def head():
-    return """
+@register.simple_tag(takes_context = True)
+def meta(context):
+    static = settings.STATIC_URL
+    page = str(context['request']).split('/')[1].capitalize()
+
+    return '''
+    <!-- Основная часть -->
     <title>Сервис отчетов Чурилово</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <link rel="stylesheet" type="text/css" href="{% static 'Server/Styles/Base.css' %}">
-    <link rel="shortcut icon" type="image/x-icon" href="{% static 'Server/Pictures/favicon.ico' %}">
-    """
-	
+    <link rel="stylesheet" type="text/css" href="''' + static + '''Server/Styles/Base.css">
+    <link rel="shortcut icon" type="image/x-icon" href="''' + static + '''Server/Pictures/favicon.ico'">
+    
+    <!-- Дополнительная часть -->
+    <link rel="stylesheet" type="text/css" href="''' + static + page + '''/Styles/''' + page + '''.css">
+    '''
