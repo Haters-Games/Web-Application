6ded80c47e5a62fdf78904db26a4d3fe3908494b
ITDarkUFO <43449316+ITDarkUFO@users.noreply.github.com>
2021-07-20 11:24:25 +0500

    Применение gitignore

--- Reports/models.py
+++ Reports/models.py
@@ -11,17 +11,15 @@ class Report(models.Model):
     greenhouse = models.BigIntegerField(verbose_name='Номер теплицы', default=0)
     report_comment = models.CharField(verbose_name='Комментарий к отчету', max_length=255)
     
+    # Methods
     def __str__(self):
-        """
-        String for representing the MyModelName object (in Admin site etc.)
-        """
         return self.field_name
     
     # Metadata
     # class Meta:
         # ordering = ["-my_field_name"]
 
-    # Methods
+    
     # def get_absolute_url(self):
          #"""
          #Returns the url to access a particular instance of MyModelName.
@@ -30,10 +28,18 @@ class Report(models.Model):
 
 
 class Menu(models.Model):
-    url = models.FilePathField(verbose_name='Применение к', default="")
+    """
+    """
+
+    # Fields
+    url = models.CharField(verbose_name='Применяется к странице', max_length=255)
     # items = models.ArrayField(
     #     models.CharField(max_length=15, blank=True, verbose_name='Название пункта'),
     #     size=8,
     #     verbose_name='Пункты меню'
     # )
     # items = models.JSONField(verbose_name='Пункты меню', default=dict)
+
+    # Methods
+    def __str__(self):
+        return self.field_name
