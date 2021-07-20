37eb83bcd1cb837b9e9f527b7655e2a16e1513ff
ITDarkUFO <43449316+ITDarkUFO@users.noreply.github.com>
2021-07-20 12:09:51 +0500

    Update models.py

--- Reports/models.py
+++ Reports/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+# from django.db.models.fields import SmallIntegerField
+import django.contrib.auth
 
 class Report(models.Model):
     """
@@ -6,18 +8,22 @@ class Report(models.Model):
     """
     
     # Fields
+    field_name = "Отчет от "
     publication_date = models.DateTimeField
     production_department = models.BigIntegerField(verbose_name='Номер производственного отделения', default=0)
     greenhouse = models.BigIntegerField(verbose_name='Номер теплицы', default=0)
     report_comment = models.CharField(verbose_name='Комментарий к отчету', max_length=255)
     
+
     # Methods
     def __str__(self):
         return self.field_name
     
+
     # Metadata
-    # class Meta:
-        # ordering = ["-my_field_name"]
+    class Meta:
+        verbose_name = "Отчет"
+        verbose_name_plural = "Отчеты"
 
     
     # def get_absolute_url(self):
@@ -32,6 +38,7 @@ class Menu(models.Model):
     """
 
     # Fields
+    field_name = "Меню для "
     url = models.CharField(verbose_name='Применяется к странице', max_length=255)
     # items = models.ArrayField(
     #     models.CharField(max_length=15, blank=True, verbose_name='Название пункта'),
@@ -43,7 +50,9 @@ class Menu(models.Model):
     # Metadata
     class Meta:
         ordering = ["-url"]
+        verbose_name = "Меню"
+        verbose_name_plural = "Меню"
 
     # Methods
     def __str__(self):
-        return self.field_name
+        return self.field_name + self.url
