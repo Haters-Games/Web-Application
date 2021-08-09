aaba2e5f6eb7097b83c8569be789294bb8999b63
Andrew Mikhalev <43449316+ITDarkUFO@users.noreply.github.com>
2021-08-09 20:07:40 +0500

    Models edit

--- Reports/admin.py
+++ Reports/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Report, Menu
+from .models import *
 
 admin.site.register(Report)
 admin.site.register(Menu)
+admin.site.register(Cultures)
+admin.site.register(GrowthStages)
--- Reports/models.py
+++ Reports/models.py
@@ -2,36 +2,6 @@ from django.db import models
 # from django.db.models.fields import SmallIntegerField
 import django.contrib.auth
 
-class Report(models.Model):
-    """
-    Класс для создания отчетов.
-    """
-    
-    # Fields
-    field_name = "Отчет от "
-    publication_date = models.DateTimeField
-    production_department = models.BigIntegerField(verbose_name='Номер производственного отделения', default=0)
-    greenhouse = models.BigIntegerField(verbose_name='Номер теплицы', default=0)
-    report_comment = models.CharField(verbose_name='Комментарий к отчету', max_length=255)
-    
-
-    # Methods
-    def __str__(self):
-        return self.field_name
-    
-
-    # Metadata
-    class Meta:
-        verbose_name = "Отчет"
-        verbose_name_plural = "Отчеты"
-
-    
-    # def get_absolute_url(self):
-         #"""
-         #Returns the url to access a particular instance of MyModelName.
-         #"""
-         #return reverse('model-detail-view', args=[str(self.id)])
-
 
 class Menu(models.Model):
     """
@@ -49,10 +19,80 @@ class Menu(models.Model):
 
     # Metadata
     class Meta:
-        ordering = ["-url"]
+        # ordering = ["-url"]
         verbose_name = "Меню"
         verbose_name_plural = "Меню"
 
     # Methods
     def __str__(self):
         return self.field_name + self.url
+
+
+# Таблицы выбора
+class Cultures(models.Model):
+    """
+    """
+
+    # Fields
+    culture_name = models.CharField(verbose_name='Название культуры', max_length=50, primary_key=True)
+
+    # Metadata
+    class Meta:
+        verbose_name = "Культуру"
+        verbose_name_plural = "Культуры"
+
+    # Methods
+    def __str__(self):
+        return self.culture_name
+
+
+class GrowthStages(models.Model):
+    """
+    """
+
+    # Fields
+    growth_stage = models.CharField(verbose_name='Стадия роста', max_length=100, primary_key=True)
+
+    # Metadata
+    class Meta:
+        verbose_name = "Стадия роста"
+        verbose_name_plural = "Стадии роста"
+
+    # Methods
+    def __str__(self):
+        return self.growth_stage
+
+
+# Модель отчета
+class Report(models.Model):
+    """
+    Класс для создания отчетов.
+    """
+    
+    # Fields
+    field_name = "Отчет №"
+    measurement_date = models.DateField(verbose_name='Дата замера')
+    production_department = models.PositiveIntegerField(verbose_name='Номер производственного отделения')
+    greenhouse = models.PositiveIntegerField(verbose_name='Номер теплицы')
+    culture = models.ForeignKey(Cultures, on_delete=models.CASCADE, verbose_name='Культура / гибрид')
+    growth_stage = models.ForeignKey(GrowthStages, on_delete=models.CASCADE, verbose_name='Стадия роста')
+    report_comment = models.CharField(verbose_name='Комментарий к отчету', max_length=255, blank=True)
+    
+
+    # Methods
+    def __str__(self):
+        return self.field_name + str(self.id) + " от " + str(self.measurement_date.day) + "." + str(self.measurement_date.month) + "." + str(self.measurement_date.year) + " (Теплица №" + str(self.greenhouse) + ")"
+    
+
+    # Metadata
+    class Meta:
+        ordering = ['greenhouse', 'measurement_date']
+        verbose_name = "Отчет"
+        verbose_name_plural = "Отчеты"
+
+    
+    # def get_absolute_url(self):
+         #"""
+         #Returns the url to access a particular instance of MyModelName.
+         #"""
+         #return reverse('model-detail-view', args=[str(self.id)])
