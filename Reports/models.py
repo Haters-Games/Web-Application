a7b7d0d1e5d6f175b58426f68768fb3f34903ba8
Andrew Mikhalev <43449316+ITDarkUFO@users.noreply.github.com>
2021-08-11 21:25:55 +0500

    Добавлены поля к модели

--- Reports/models.py
+++ Reports/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 # from django.db.models.fields import SmallIntegerField
-import django.contrib.auth
+# import django.contrib.auth
 
 
 class Menu(models.Model):
@@ -69,7 +70,8 @@ class Report(models.Model):
     Класс для создания отчетов.
     """
     
-    # Fields
+    # Поля модели
+    # -- Заглавная часть --
     field_name = "Отчет №"
     measurement_date = models.DateField(verbose_name='Дата замера')
     production_department = models.PositiveIntegerField(verbose_name='Номер производственного отделения')
@@ -77,14 +79,30 @@ class Report(models.Model):
     culture = models.ForeignKey(Cultures, on_delete=models.CASCADE, verbose_name='Культура / гибрид')
     growth_stage = models.ForeignKey(GrowthStages, on_delete=models.CASCADE, verbose_name='Стадия роста')
     report_comment = models.CharField(verbose_name='Комментарий к отчету', max_length=255, blank=True)
-    
 
-    # Methods
+    # -- Обеспеченность растений светом --
+    solar_radiation_per_day = models.PositiveIntegerField(verbose_name='Приход естественной солнечной радиации за сутки')
+    light_intensity = models.PositiveIntegerField(verbose_name='Интенсивность естественного света')
+    light_intensity_per_day = models.PositiveIntegerField(verbose_name='Интенсивность естественного света (за световой день)')
+    glazing_throughput = models.PositiveIntegerField(validators=[MaxValueValidator(100)], verbose_name='Пропускная способность остекления (в процентах)')
+    sunrise_time = models.TimeField(verbose_name='Время естественного восхода Солнца')
+    sunset_time = models.TimeField(verbose_name='Время естественного захода Солнца')
+    lighting_system_power = models.PositiveIntegerField(verbose_name='Мощность системы досвечивания')
+    lighting_system_turn_on_time = models.TimeField(verbose_name='Время включения досвечивания')
+    lighting_system_turn_off_time = models.TimeField(verbose_name='Время отключения досвечивания')
+    lighting_system_operating_time = models.PositiveIntegerField(verbose_name='Количество часов работы системы досвечивания (50% вкл и откл берутся как 0,5 за 1 полный час)')
+    unlit_light_points = models.PositiveIntegerField(verbose_name='Количество негорящих светоточек')
+    # PAR_for_hour_from_lighting_system = models.PositiveIntegerField(verbose_name='Приход ФАР за 1 час от системы досвечивания') - формула
+    # PAR_per_day_from_lighting_system = models.PositiveIntegerField(verbose_name='Приход ФАР за 1 сутки от системы досвечивания') - формула
+    # total_PAR = models.PositiveIntegerField(verbose_name='Суммарная ФАР с досветкой') - формула
+
+
+    # Методы
     def __str__(self):
         return self.field_name + str(self.id) + " от " + str(self.measurement_date.day) + "." + str(self.measurement_date.month) + "." + str(self.measurement_date.year) + " (Теплица №" + str(self.greenhouse) + ")"
     
 
-    # Metadata
+    # Метадата
     class Meta:
         ordering = ['greenhouse', 'measurement_date']
         verbose_name = "Отчет"
